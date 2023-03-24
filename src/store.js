30de0975e9fb1811bdb37d16dc55a8b43b26bf0a
Albrecht-Albi <albrecht.bar1@gmail.com>
2023-03-24 12:25:02 +0100

    persist tasks in LocalStorage

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
+import { tasksSaga } from "./features/tasks/tasksSaga";
 import tasksReducer from "./features/tasks/tasksSlice";
-import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
+
 const store = configureStore({
   reducer: {
     tasks: tasksReducer,
@@ -11,6 +12,6 @@ const store = configureStore({
   middleware: [sagaMiddleware],
 });
 
-sagaMiddleware.run(rootSaga)
+sagaMiddleware.run(tasksSaga);
 
 export default store;
