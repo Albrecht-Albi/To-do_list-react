8e2d3c55c8545fe6d21bd20b9b02784b7e00e4a2
Albrecht-Albi <albrecht.bar1@gmail.com>
2023-03-24 12:28:05 +0100

    add navigation

---
+++ src/features/Navigation/index.js
@@ -0,0 +1,17 @@
+import { toAuthor, toTasks } from "../../routes";
+import { StyledItem, StyledNavLink, StyledUl } from "./styled";
+
+const Navigation = () => (
+  <nav>
+    <StyledUl>
+      <StyledItem>
+        <StyledNavLink to={toTasks()}>Zadania</StyledNavLink>
+      </StyledItem>
+      <StyledItem>
+        <StyledNavLink to={toAuthor()}>O autorze</StyledNavLink>
+      </StyledItem>
+    </StyledUl>
+  </nav>
+);
+
+export default Navigation;
