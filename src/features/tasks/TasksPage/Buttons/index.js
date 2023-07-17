7c6eb31bb4f710d8891572f8f785f51dda7dad19
Albrecht-Albi <albrecht.bar1@gmail.com>
2023-07-17 09:26:15 +0200

    formating code

--- src/features/tasks/TasksPage/Buttons/index.js
+++ src/features/tasks/TasksPage/Buttons/index.js
@@ -29,6 +29,4 @@ const Buttons = () => {
   );
 };
 
-
-
 export default Buttons;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from 'styled-components';
-import { App } from './App';
-import { Provider } from 'react-redux'
-import { GlobalStyle } from './GlobalStyle';
-import { theme } from './theme'
-import store from './store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "styled-components";
+import { App } from "./App";
+import { Provider } from "react-redux"
+import { GlobalStyle } from "./GlobalStyle";
+import { theme } from "./theme"
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
